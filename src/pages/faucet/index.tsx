e864f41f064e38c19f225c47a59a822fbbd326da
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-01-21 00:44:10 +0530

    chore: added alert if wallet not connected

--- src/pages/faucet/index.tsx
+++ src/pages/faucet/index.tsx
@@ -6,7 +6,9 @@ import Layout from "@/components/layout/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import { useFaucet } from "@/hooks/useFaucet";
+import { useAccount } from "wagmi";
 function Faucet() {
+  const account = useAccount();
   const [loadingStatus, setLoadingStatus] = useState({
     loading: false,
     success: false,
@@ -102,6 +104,7 @@ function Faucet() {
               <button
                 onClick={() => claimFaucet()}
                 className="bg-gradient-to-r from-blue-800 to-purple-800 text-white py-2 px-4 rounded-md hover:scale-105 transition duration-200"
+                disabled={!account.isConnected}
               >
                 Get Tokens
               </button>
@@ -113,6 +116,29 @@ function Faucet() {
                 ""
               )}
             </div>
+            {account.isConnected == false ? (
+              <div
+                role="alert"
+                className="alert alert-warning absolute top-3 w-[40%] mx-auto left-0 right-0"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
+                <span>Connect your wallet!</span>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         ) : (
           ""
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,7 +6,6 @@ import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import axios from "axios";
-import User from "@/db/index";
 import Layout from "@/components/layout/Layout";
 function Home() {
   const router = useRouter();
@@ -136,7 +135,7 @@ function Home() {
               <button
                 onClick={buyCourse}
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mx-auto mt-4"
-                disabled={!loadingStatus.idle}
+                disabled={!loadingStatus.idle || !account.isConnected}
               >
                 Buy Harkirat Course
               </button>
@@ -219,6 +218,29 @@ function Home() {
               </svg>
               <span>Your transaction is successfull</span>
             </div>
+          )}
+          {account.isConnected == false ? (
+            <div
+              role="alert"
+              className="alert alert-warning absolute top-3 w-[40%] mx-auto left-0 right-0"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="stroke-current shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                />
+              </svg>
+              <span>Connect your wallet!</span>
+            </div>
+          ) : (
+            ""
           )}
         </Layout>
       </main>
